fc1c7691b1057fbc2d8675d69bc8c778921ae665
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-08-12 11:16:55 -0500

    feature se hace la suma

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Interactuemos con el usuario
+        Scanner LeerTeclado = new Scanner(System.in);
+
+        System.out.println("sumaremos dos numeros");
+        System.out.println("☆*: .｡. o(≧▽≦)o .｡.:*☆");
+
+        try {
+            System.out.print("Digita el primer numero entero: ");
+            Integer numeroUno = LeerTeclado.nextInt();
+            System.out.print("Digita el segundo numero entero: ");
+            Integer numeroDos = LeerTeclado.nextInt();
+
+
+            //Llamando a la funcion que suma los dos numeros
+            Integer resultado = sumarDosNumeros(numeroUno, numeroDos);
+            System.out.println("el resultado de la suma es: " + resultado);
+        } catch (InputMismatchException error) {
+            System.out.println("✖️ ERROR: Utilizaste una entrada prohibida");
+
+        } catch (Exception error) {
+            System.out.println("✖️ UPS!: detectamos una operacion indebida, sal de aquí pirata" + error.getMessage());
+
+        } finally {
+            System.out.println("gracias, hasta pronto 😁");
+            LeerTeclado.close();
         }
+
     }
+    public static Integer sumarDosNumeros(Integer numeroUno, Integer numeroDos) {
+
+        if (numeroUno == 0 && numeroDos == 0) {
+            throw new ArithmeticException("\n No puedes sumar dos ceros consecutivos ");
+
+        } else if
+        (numeroUno <0 && numeroDos <0) {
+            throw new ArithmeticException("\n No puedes sumar numeros negativos consecutivos ");
+
+        }
+        return numeroUno + numeroDos;
+    }
+
 }
